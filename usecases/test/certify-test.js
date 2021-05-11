886dc9a7538191fc352283b905eab4b1f613eff9
step <stefano.pellegrini@smartshaped.com>
2021-05-11 12:36:28 +0200

    slither-analyzer: conform to mixedCase solidity standard and turn public functions into external

--- usecases/test/certify-test.js
+++ usecases/test/certify-test.js
@@ -10,14 +10,14 @@ describe("Certify", function() {
     const certify = await Certify.connect(addr1).deploy(name, url);
 
     await certify.deployed();
-    expect(await certify.certification_body_name()).to.equal(name);
+    expect(await certify.certificationBodyName()).to.equal(name);
 
     const cert = 'plastic-free'
     const expiry = parseInt(Date.now() / 1000) + 3600
     await certify.issue(addr2.address, cert, expiry);
-    expect(await certify.num_certificates(addr2.address)).to.equal(1);
+    expect(await certify.numCertificates(addr2.address)).to.equal(1);
     expect(await certify.connect(addr2).prove(cert));
-    expect(await certify.connect(addr3).is_valid(addr2.address, cert)).to.be.true;
+    expect(await certify.connect(addr3).isValid(addr2.address, cert)).to.be.true;
     // TODO test that expired or non-existent certificates cannot be proven
   });
 });
